84bd25f1e942ef0c269813f1907150dffd7f3e14
Eduardo Zampiere <eduardo@mytime.com.br>
2020-06-19 00:45:50 -0300

    add graphql

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { StyledHeader, Container, Item, Brand, Select, Button, ContainerBottom } from './style';
-import { useData } from '../../context/Data';
-function Header() {
+import {
+	StyledHeader,
+	Container,
+	Item,
+	Brand,
+	Select,
+	Button,
+	ContainerBottom,
+} from './style';
 
-	const { updateData } = useData();
+import { useData } from '../../context/Data';
+
+function Header() {
+	const { updateData, setFilter } = useData();
+	function changeHandler(e) {
+		const value = e.currentTarget.selectedOptions[0].attributes.value.nodeValue;
+		const type = e.currentTarget.selectedOptions[0].attributes.type.nodeValue;
+		let obj = {};
+		obj[type] = value;
+		setFilter(obj);
+	}
 
 	return (
 		<StyledHeader>
@@ -12,64 +28,129 @@ function Header() {
 					<Brand href="/">Covid-19 | Painel</Brand>
 				</Item>
 				<ContainerBottom>
-
 					<Item>
-						<Select>
-							<option value={0} type={0}>Todos</option>
+						<Select onChange={changeHandler}>
+							<option value="" type="">
+								Todos
+							</option>
 							<optgroup label="Regiões">
-							<option value="Sudeste" type="regiao">Sudeste</option>
-								<option value="Nordeste" type="regiao">Nordeste</option>
-								<option value="Centro-Oeste" type="regiao">Centro-Oeste</option>
-								<option value="Sul" type="regiao">Sul</option>
-								<option value="Norte" type="regiao">Norte</option>
+								<option value="Sudeste" type="region">
+									Sudeste
+								</option>
+								<option value="Nordeste" type="region">
+									Nordeste
+								</option>
+								<option value="Centro-Oeste" type="region">
+									Centro-Oeste
+								</option>
+								<option value="Sul" type="region">
+									Sul
+								</option>
+								<option value="Norte" type="region">
+									Norte
+								</option>
 							</optgroup>
 
 							<optgroup label="Centro-Oeste">
-							<option value="DF" type="estado">DF - Distrito Federal</option>
-								<option value="GO" type="estado">GO - Goias</option>
-								<option value="MS" type="estado">MS - Mato Grosso do Sul</option>
-								<option value="MT" type="estado">MT - Mato Grosso</option>
+								<option value="DF" type="state">
+									DF - Distrito Federal
+								</option>
+								<option value="GO" type="state">
+									GO - Goias
+								</option>
+								<option value="MS" type="state">
+									MS - Mato Grosso do Sul
+								</option>
+								<option value="MT" type="state">
+									MT - Mato Grosso
+								</option>
 							</optgroup>
 
 							<optgroup label="Nordeste">
-							<option value="BA" type="estado">BA - Bahia</option>
-								<option value="AL" type="estado">AL - Alagoas</option>
-								<option value="PE" type="estado">PE - Pernambuco</option>
-								<option value="RN" type="estado">RN - Rio Grande do Norte</option>
-								<option value="SE" type="estado">SE - Sergipe</option>
-								<option value="CE" type="estado">CE - Ceará</option>
-								<option value="PB" type="estado">PB - Paraíba</option>
-								<option value="PI" type="estado">PI - Piauí</option>
-								<option value="MA" type="estado">MA - Maranhão</option>
+								<option value="BA" type="state">
+									BA - Bahia
+								</option>
+								<option value="AL" type="state">
+									AL - Alagoas
+								</option>
+								<option value="PE" type="state">
+									PE - Pernambuco
+								</option>
+								<option value="RN" type="state">
+									RN - Rio Grande do Norte
+								</option>
+								<option value="SE" type="state">
+									SE - Sergipe
+								</option>
+								<option value="CE" type="state">
+									CE - Ceará
+								</option>
+								<option value="PB" type="state">
+									PB - Paraíba
+								</option>
+								<option value="PI" type="state">
+									PI - Piauí
+								</option>
+								<option value="MA" type="state">
+									MA - Maranhão
+								</option>
 							</optgroup>
 
 							<optgroup label="Norte">
-							<option value="AM" type="estado">AM - Amazonas</option>
-								<option value="AC" type="estado">AC - Acre</option>
-								<option value="PA" type="estado">PA - Pará</option>
-								<option value="TO" type="estado">TO - Tocantins</option>
-								<option value="AP" type="estado">AP - Amapá</option>
-								<option value="RO" type="estado">RO - Rondônia</option>
-								<option value="RR" type="estado">RR - Roraima</option>
+								<option value="AM" type="state">
+									AM - Amazonas
+								</option>
+								<option value="AC" type="state">
+									AC - Acre
+								</option>
+								<option value="PA" type="state">
+									PA - Pará
+								</option>
+								<option value="TO" type="state">
+									TO - Tocantins
+								</option>
+								<option value="AP" type="state">
+									AP - Amapá
+								</option>
+								<option value="RO" type="state">
+									RO - Rondônia
+								</option>
+								<option value="RR" type="state">
+									RR - Roraima
+								</option>
 							</optgroup>
 
 							<optgroup label="Sudeste">
-							<option value="SP" type="estado">SP - São Paulo</option>
-								<option value="RJ" type="estado">RJ - Rio de Janeiro</option>
-								<option value="ES" type="estado">ES - Espírito Santo</option>
-								<option value="MG" type="estado">MG - Minas Gerais</option>
+								<option value="SP" type="state">
+									SP - São Paulo
+								</option>
+								<option value="RJ" type="state">
+									RJ - Rio de Janeiro
+								</option>
+								<option value="ES" type="state">
+									ES - Espírito Santo
+								</option>
+								<option value="MG" type="state">
+									MG - Minas Gerais
+								</option>
 							</optgroup>
 
 							<optgroup label="Sul">
-							<option value="RS" type="estado">RS - Rio Grande do Sul</option>
-								<option value="PR" type="estado">PR - Paraná</option>
-								<option value="SC" type="estado">SC - Santa Catarina</option>
+								<option value="RS" type="state">
+									RS - Rio Grande do Sul
+								</option>
+								<option value="PR" type="state">
+									PR - Paraná
+								</option>
+								<option value="SC" type="state">
+									SC - Santa Catarina
+								</option>
 							</optgroup>
 						</Select>
 					</Item>
 
 					<Item float="right">
-						<Button onClick={updateData} >Atualizar</Button>
+						<Button onClick={updateData}>Atualizar</Button>
 					</Item>
 				</ContainerBottom>
 			</Container>
--- client/src/context/Data.js
+++ client/src/context/Data.js
@@ -1,31 +1,41 @@
-import React, { createContext, useState, useContext, useEffect, useCallback } from 'react';
-import api from '../api';
+import React, {
+	createContext,
+	useState,
+	useContext,
+	useEffect,
+	useCallback,
+} from 'react';
+import api from '../graphql';
 import momentjs from 'moment';
-import { populations } from '../config/populations.json' 
+import { populations } from '../config/populations.json';
 
 const DataContext = createContext();
 
-export default function DataProvider({children}){
+export default function DataProvider({ children }) {
 	const [data, setData] = useState(null);
+	const [filter, setFilter] = useState({});
 
 	const updateData = async () => {
 		const responseUpdate = await api.update();
-		await loadData();
-		if(responseUpdate.data.data.updateCases){
+		await loadData(filter);
+		if (responseUpdate.data.updateCases) {
 			alert('Ok');
-		}
-		else{
+		} else {
 			alert('Houve um erro!');
 		}
-	}
+	};
 
-	const loadData = useCallback( async () => {
-		const response = await api.getData();
-		const responseData = response.data.data.cases;
-		organizeData(responseData);
+	const loadData = useCallback(async (filter) => {
+		try {
+			const response = await api.getData(filter);
+			const responseData = response.data.cases;
+			organizeData(responseData);
+		} catch (err) {
+			console.log(err);
+		}
 	}, []);
 
-	const organizeData = useCallback( (responseData) => {
+	const organizeData = useCallback((responseData) => {
 		const totalCase = {
 			perHundredThousand: {},
 			perState: {},
@@ -45,10 +55,10 @@ export default function DataProvider({children}){
 			lastDeads: 0,
 			toDouble: 0,
 			projectionDays: 30,
-			lethality: null
-		}
+			lethality: null,
+		};
 
-		for(let dayCase of responseData){
+		for (let dayCase of responseData) {
 			const state = dayCase.state;
 			const date = dayCase.date;
 			const region = dayCase.region;
@@ -59,70 +69,70 @@ export default function DataProvider({children}){
 			totalCase.deads += newDeaths;
 			totalCase.infecteds += newCases;
 
-			if(totalCase.infecteds <= 0 && totalCase.deads <= 0) continue;
+			if (totalCase.infecteds <= 0 && totalCase.deads <= 0) continue;
 
 			totalCase.lastDay = date;
 
-			if(!totalCase.firstDeath && totalCase.deads){
+			if (!totalCase.firstDeath && totalCase.deads) {
 				totalCase.firstDeath = date;
 			}
 
-			if(!totalCase.firstCase){
+			if (!totalCase.firstCase) {
 				totalCase.firstCase = date;
 			}
 
 			//States per region
-			if(!totalCase.statePerRegion[region]){
-				totalCase.statePerRegion[region] = {}
+			if (!totalCase.statePerRegion[region]) {
+				totalCase.statePerRegion[region] = {};
 			}
-			if(!totalCase.statePerRegion[region][state]){
+			if (!totalCase.statePerRegion[region][state]) {
 				totalCase.statePerRegion[region][state] = true;
 			}
 
 			//Cases per day
-			if(!totalCase.perDay[date]){
-				totalCase.perDay[date] = {}
+			if (!totalCase.perDay[date]) {
+				totalCase.perDay[date] = {};
 			}
 
 			totalCase.perDay[date].deads = totalCase.deads;
 			totalCase.perDay[date].infecteds = totalCase.infecteds;
 
 			//New cases per day
-			if(!totalCase.perNewDay[date]){
+			if (!totalCase.perNewDay[date]) {
 				totalCase.perNewDay[date] = {
 					deads: 0,
-					infecteds: 0
+					infecteds: 0,
 				};
 			}
 			totalCase.perNewDay[date].deads += newDeaths;
 			totalCase.perNewDay[date].infecteds += newCases;
 
 			//Cases per state
-			if(!totalCase.perState[state]){
+			if (!totalCase.perState[state]) {
 				totalCase.perState[state] = {
 					deads: 0,
-					infecteds: 0
+					infecteds: 0,
 				};
 			}
 			totalCase.perState[state].deads += newDeaths;
 			totalCase.perState[state].infecteds += newCases;
 
 			//Cases per region
-			if(!totalCase.perRegion[region]){
+			if (!totalCase.perRegion[region]) {
 				totalCase.perRegion[region] = {
 					infecteds: 0,
-					deads: 0
+					deads: 0,
 				};
 			}
 			totalCase.perRegion[region].infecteds += newCases;
 			totalCase.perRegion[region].deads += newDeaths;
-		
+
 			//New cases per week
-			if(!totalCase.perWeek[weekNumber]){
+			if (!totalCase.perWeek[weekNumber]) {
 				totalCase.perWeek[weekNumber] = {
 					infecteds: 0,
-					deads: 0
-				}
+					deads: 0,
+				};
 			}
 			totalCase.perWeek[weekNumber].infecteds += newCases;
 			totalCase.perWeek[weekNumber].deads += newDeaths;
@@ -130,94 +140,101 @@ export default function DataProvider({children}){
 
 		const arr = [];
 		//Lethality per day
-		for(let day in totalCase.perDay){
+		for (let day in totalCase.perDay) {
 			const dayCase = totalCase.perDay[day];
 			const infecteds = dayCase.infecteds;
 			const deads = dayCase.deads;
 			arr.push(infecteds);
-			
-			if(deads > 0){
-				totalCase.perLethality[day] = (deads/infecteds) * 100;
+
+			if (deads > 0) {
+				totalCase.perLethality[day] = (deads / infecteds) * 100;
 			}
 		}
 
 		//Double infecteds
 		let aux = 0;
-		for(let i = arr.length - 1; i >= 0; i--){
+		for (let i = arr.length - 1; i >= 0; i--) {
 			const infecteds = arr[i];
-			if(aux == 0) aux = infecteds;
+			if (aux == 0) aux = infecteds;
 
-			if(infecteds <= aux / 2){
+			if (infecteds <= aux / 2) {
 				break;
 			}
 			totalCase.toDouble++;
 		}
-		
+
 		//Per hundred thousand
-		for(let state in totalCase.perState){
+		for (let state in totalCase.perState) {
 			const dayCase = totalCase.perState[state];
 			totalCase.perHundredThousand[state] = {
 				infecteds: (dayCase.infecteds / populations[state]) * 100000,
-				deads: (dayCase.deads / populations[state]) * 100000
-			}
+				deads: (dayCase.deads / populations[state]) * 100000,
+			};
 		}
 
 		//Projection
-		totalCase.lethality = (totalCase.deads/totalCase.infecteds);
+		totalCase.lethality = totalCase.deads / totalCase.infecteds;
 		let toDouble_mirror = 1;
-		for(let i = 1; i <= totalCase.projectionDays; i++){
-			let newInfections = parseInt( aux * (Math.pow(2, toDouble_mirror/totalCase.toDouble)));
+		for (let i = 1; i <= totalCase.projectionDays; i++) {
+			let newInfections = parseInt(
+				aux * Math.pow(2, toDouble_mirror / totalCase.toDouble)
+			);
 			toDouble_mirror++;
-			const futureDay = momentjs(totalCase.lastDay, "YYYY-MM-DD").add(i, 'd').format('YYYY-MM-DD');
-			
+			const futureDay = momentjs(totalCase.lastDay, 'YYYY-MM-DD')
+				.add(i, 'd')
+				.format('YYYY-MM-DD');
+
 			totalCase.projection[futureDay] = {
 				infecteds: newInfections,
-				deads: parseInt(newInfections * totalCase.lethality)					
-			}
+				deads: parseInt(newInfections * totalCase.lethality),
+			};
 		}
 
 		totalCase.lastInfecteds = totalCase.perNewDay[totalCase.lastDay].infecteds;
 		totalCase.lastDeads = totalCase.perNewDay[totalCase.lastDay].deads;
 		setData(totalCase);
-
 	}, []);
 
 	const formatNumber = (value) => {
-		return new Intl.NumberFormat('pt-BR', {
-
-		}).format(value);
-	}
+		return new Intl.NumberFormat('pt-BR', {}).format(value);
+	};
 
 	const formatPercent = (value) => {
 		return new Intl.NumberFormat('pt-BR', {
 			style: 'percent',
 			minimumFractionDigits: 2,
-			maximumFractionDigits: 2
+			maximumFractionDigits: 2,
 		}).format(value);
-	}
+	};
 
 	const formatDate = (date) => {
 		return date.split('-').reverse().join('/');
-		
-	}
+	};
 
-	useEffect( () => {
-		( async () => {
-			await loadData();			
+	useEffect(() => {
+		(async () => {
+			await loadData(filter);
 		})();
-	}, [loadData]);
+	}, [loadData, filter]);
 
 	return (
-		<DataContext.Provider value={{
-			data, setData,
-			formatNumber, formatPercent,
-			formatDate, updateData
-		}}>
+		<DataContext.Provider
+			value={{
+				data,
+				setData,
+				filter,
+				setFilter,
+				formatNumber,
+				formatPercent,
+				formatDate,
+				updateData,
+			}}
+		>
 			{children}
 		</DataContext.Provider>
-	)
+	);
 }
 
-export function useData(){
+export function useData() {
 	return useContext(DataContext);
 }
