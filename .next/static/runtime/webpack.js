f14e93ab96e5a9d002c3ae2b60c77f639d2a7163
Eduardo Zampiere <eduardo@mytime.com.br>
2020-06-21 18:06:23 -0300

    bug fix

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "97d790116727d7b63eaa";
+/******/ 	var hotCurrentHash = "26e429fea45cdfca3aaa";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/services/apollo.js
+++ src/services/apollo.js
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import fetch from 'isomorphic-unfetch';
-
+import Head from 'next/head';
 export function withApollo(PageComponent) {
 	const WithApollo = ({ apolloClient, apolloState, ...pageProps }) => {
 		const client = apolloClient || initApolloClient(apolloState);
@@ -19,6 +19,31 @@ export function withApollo(PageComponent) {
 		const apolloClient = (ctx.apolloClient = initApolloClient);
 
 		let pageProps = {};
+
+		if (PageComponent.getInicialProps) {
+			pageProps = await PageComponent.getInicialProps(ctx);
+		}
+
+		//On server
+		if (typeof window === 'undefined') {
+			if (ctx.res && ctx.res.finished) {
+				return pageProps;
+			}
+			try {
+				const { getDataFromTree } = await import('@apollo/react-ssr');
+				await getDataFromTree(
+					<AppTree
+						pageProps={{
+							...pageProps,
+							apolloClient,
+						}}
+					/>
+				);
+			} catch (e) {
+				console.log(e);
+			}
+			Head.rewind();
+		}
 
 		const apolloState = apolloClient.cache.extract();
 
